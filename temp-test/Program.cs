b3d01f7e562ecb0964729d40405fcc0cdd3c4af7
Gleriston Castro <xpanium@gmail.com>
2025-11-22 00:29:46 -0300

    test: verify Microsoft.OpenApi.Models namespace

---
+++ temp-test/Program.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.OpenApi.Models;
+
+Console.WriteLine("Microsoft.OpenApi.Models namespace OK!");
+Console.WriteLine($"OpenApiInfo type exists: {typeof(OpenApiInfo).Name}");
+Console.WriteLine($"OpenApiContact type exists: {typeof(OpenApiContact).Name}");
+Console.WriteLine($"OpenApiLicense type exists: {typeof(OpenApiLicense).Name}");
